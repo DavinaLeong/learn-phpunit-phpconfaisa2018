260a9465637ca0c052892a0532d0b00b2044216e
Davina Leong <davina.leong.sy@gmail.com>
2018-09-27 22:38:15 +0800

    Tested for routes not allowed to have identical cities

---
+++ tests/route/RouteTest.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+namespace TicketToRide;
+
+use PHPUnit\Framework\TestCase;
+
+final class RouteTest extends TestCase
+{
+    private $city;
+    private $citySameName;
+    private $cityDiffName;
+
+    private $route;
+
+    public function setUp()
+    {
+        $this->city         = new City("Hello City");
+        $this->citySameName = new City("Hello City");
+        $this->cityDiffName = new City("Hello Metropolis");
+
+        $this->route        = new Route($this->city, $this->cityDiffName, Color::purple(), Length::two());
+    }
+
+    public function testNamesCantBeSame()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Route($this->city, $this->citySameName, Color::red(), Length::one());
+    }
+
+} //end RouteTest
