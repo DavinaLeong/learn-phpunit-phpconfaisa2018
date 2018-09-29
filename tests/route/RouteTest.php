6a6ad8c96236b514e0051b18aedd984f003cfc62
Davina Leong <davina.leong.sy@gmail.com>
2018-09-29 20:40:13 +0800

    Used a data provider for checking a route's color

--- tests/route/RouteTest.php
+++ tests/route/RouteTest.php
@@ -29,22 +29,26 @@ final class RouteTest extends TestCase
 
         $this->routePurple  = new Route($this->city, $this->cityDiffName,
             Color::purple(), Length::two());
+        $this->routeGray    = new Route($this->city, $this->cityDiffName,
+            Color::wildcard(), Length::two());
 
-        $this->cardsPurple        = new CardCollection();
+        $this->cardsPurple  = new CardCollection();
         $this->cardsPurple->add(Card::purple());
         $this->cardsPurple->add(Card::purple());
     }
 
+    //#region Tests
     public function testCitiesCantBeSame()
     {
         $this->expectException(InvalidArgumentException::class);
         new Route($this->city, $this->citySameName, Color::red(), Length::one());
     }
 
-    public function testRouteHasColor()
+    /**
+     * @dataProvider routeColorProvider
+     */
+    public function testRouteHasColor($expected, $actual)
     {
-        $expected   = Color::purple();
-        $actual     = $this->routePurple->color();
         $this->assertEquals($expected, $actual);
     }
 
@@ -63,14 +67,82 @@ final class RouteTest extends TestCase
         $this->assertTrue($expected);
     }
 
-    public function testWildcardClaim()
+    public function testWildcardClaimable()
     {
-        $this->markTestIncomplete();
+        $checker    = new RouteClaimChecker();
+        $expected   = $checker->canClaimRoute($this->routeGray, $this->cardsPurple);
+
+        $this->assertTrue($expected);
     }
 
     public function testCalculateScore()
     {
         $this->markTestIncomplete();
     }
+    //#endregion
+
+
+    //#region Data Providers
+    public function routeColorProvider()
+    {
+        $city         = new City("Hello City");
+        $cityDiffName = new City("Hello Metropolis");
+
+        $data = [
+            'purple' => [
+                new Route($city, $cityDiffName,
+                    Color::purple(), Length::one()),
+                Color::purple()
+            ],
+            'white' => [
+                new Route($city, $cityDiffName,
+                    Color::white(), Length::one()),
+                Color::white()
+            ],
+            'blue' => [
+                new Route($city, $cityDiffName,
+                    Color::blue(), Length::one()),
+                Color::blue()
+            ],
+            'yellow' => [
+                new Route($city, $cityDiffName,
+                    Color::yellow(), Length::one()),
+                Color::yellow()
+            ],
+            'orange' => [
+                new Route($city, $cityDiffName,
+                    Color::orange(), Length::one()),
+                Color::orange()
+            ],
+            'black' => [
+                new Route($city, $cityDiffName,
+                    Color::black(), Length::one()),
+                Color::black()
+            ],
+            'red' => [
+                new Route($city, $cityDiffName,
+                    Color::red(), Length::one()),
+                Color::red()
+            ],
+            'green' => [
+                new Route($city, $cityDiffName,
+                    Color::green(), Length::one()),
+                Color::green()
+            ],
+            'wildcard' => [
+                new Route($city, $cityDiffName,
+                    Color::wildcard(), Length::one()),
+                Color::wildcard()
+            ]
+        ];
+
+        foreach ($data as $key=>$val) {
+            $data[$key][0] = $val[0]->color();
+        }
+        var_dump($data);
+
+        return $data;
+    }
+    //#endregion
 
 } //end RouteTest
